12a28140f09c9b24eb8ca8179d1fd5aa4b7cc7b7
masahu84 <mshumanes@egmail.com>
2025-12-06 08:01:47 +0100

    cambios package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Habilita SSR para rutas dinámicas
+  output: "server", // Habilita SSR para rutas dinámicas
+  adapter: node({
+    mode: "standalone", // o 'server', pero 'standalone' suele ir bien
+  }),
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });
